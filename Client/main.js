ac2bf4b932e0df449a358a9c931d444335bbb5a4
Philo Kaulkin <Phil0nator@users.noreply.github.com>
2020-06-11 19:46:51 -0700

    Jquery message input

--- Client/main.js
+++ Client/main.js
@@ -7,21 +7,21 @@ try{
     const { app, BrowserWindow } = require('electron')
 
     function createWindow () {
-    // Create the browser window.
-    const win = new BrowserWindow({
-        width: 1920,
-        height: 1080,
-        frame:true,
-        webPreferences: {
-        nodeIntegration: true
-        }
-    })
+        // Create the browser window.
+        const win = new BrowserWindow({
+            width: 1920,
+            height: 1080,
+            frame:false,
+            webPreferences: {
+            nodeIntegration: true
+            }
+        })
 
-    // and load the index.html of the app.
-    win.loadFile('index.html')
+        // and load the index.html of the app.
+        win.loadFile('index.html')
 
-    // Open the DevTools.
-    win.webContents.openDevTools()
+        // Open the DevTools.
+        
     }
 
     // This method will be called when Electron has finished
@@ -31,19 +31,19 @@ try{
 
     // Quit when all windows are closed.
     app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+        // On macOS it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
     })
 
     app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
     })
 
     // In this file you can include the rest of your app's specific main process
